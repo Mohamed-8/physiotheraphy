6db462c710533c16cac1a99b0355ea3aee95b19b
Mohamed-8 <mohamedibrahim8996@gmail.com>
2024-02-21 10:41:52 +0530

    initial commmit

---
+++ app/about/page.js
@@ -0,0 +1,86 @@
+import Image from "next/image";
+import "../../public/page.css";
+
+export default function About() {
+  return (
+    <diV>
+      <div className="elementor-widget-container">
+        <h2>About Me</h2>
+      </div>
+      <div className="elementor-container">
+        <Image
+          src="/about/about.jpg"
+          width={700}
+          height={500}
+          alt="about section"
+        />
+        <div>
+          <h5 className="dashed">About our therapies</h5>
+          <h3>
+            Explore our psychology <br /> section!
+          </h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
+            tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
+          </p>
+          <hr className="elementor-seperator" />
+          <div className="elementor-iconbox-content">
+            <span>Balance & self-care</span>
+            <span>Trasformation</span>
+          </div>
+          <p>
+            Cursus metus aliquam eleifend mi. Sollicitudin aliquam ultrices
+            sagittis orci a scelerisque purus semper. Varius vel pharetra vel
+            turpis nunc eget lorem dolor sed. Libero justo laoreet sit amet. Dui
+            id ornare arcu odio ut sem nulla pharetra.
+          </p>
+          <hr className="elementor-seperator" />
+        </div>
+      </div>
+
+      <div className="elementor-about-description">
+        <hr className="elementor-about-title" />
+        <br />
+        <span>Our process</span>
+        <p>
+          We improve your health with our <br /> full-range therapies
+        </p>
+      </div>
+
+      <div className="elementor-default">
+        <div className="elementor-default-size">
+          <h4>
+            01. Initial <br /> Assessment
+          </h4>
+          <p>
+            Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
+          </p>
+        </div>
+        <div className="elementor-default-size">
+          <h4>
+            02. Setting <br /> goals
+          </h4>
+          <p>
+            Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
+          </p>
+        </div>
+        <div className="elementor-default-size">
+          <h4>
+            03. Implementation <br /> of theraphy
+          </h4>
+          <p>
+            Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
+          </p>
+        </div>
+        <div className="elementor-default-size">
+          <h4>
+            04. Continuous <br /> evaluation
+          </h4>
+          <p>
+            Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
+          </p>
+        </div>
+      </div>
+    </diV>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,95 +1,301 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import "../public/page.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
+    <div>
+      <div class="topnav">
+        <a>Home</a>
+        <a>About</a>
+        <a>Pages</a>
+        <a>Blog</a>
+        <a>Therapies</a>
+        <a>Contact Me</a>
+      </div>
+      <div className="elementor-container">
+        <Image
+          src="/health.jpg"
+          width={700}
+          height={500}
+          alt="Picture of health"
+        />
         <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+          <h5 className="dashed">About our therapies</h5>
+          <h3>Take care of your mind. Explore our psychology section!</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
+            tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
+          </p>
+          <hr className="elementor-seperator" />
+        </div>
+      </div>
+      <div className="elementor-description">
+        <hr className="elementor-title" />
+        <br />
+        <span>Transform your mind</span>
+        <p>
+          Find balance & well-being with our <br /> personalized therapies
+        </p>
+      </div>
+      <div className="elementor-inner-section">
+        <div className="elementor-element">
+          <h3>Individual Theraphy</h3>
+          <p>Ullamcorper mattis, pulvinar dapibus leo.</p>
+        </div>
+        <div className="elementor-element">
+          <h3>Family Theraphy</h3>
+          <p>Ullamcorper mattis, pulvinar dapibus leo.</p>
+        </div>
+        <div className="elementor-element">
+          <h3>Couples Theraphy</h3>
+          <p>Ullamcorper mattis, pulvinar dapibus leo.</p>
+        </div>
+        <div className="elementor-element">
+          <h3>Coaching Services</h3>
+          <p>Ullamcorper mattis, pulvinar dapibus leo.</p>
         </div>
       </div>
 
-      <div className={styles.center}>
+      <div className="elementor-container">
+        <div className="elementor-widget-wrap">
+          <Image
+            src="/office.jpg"
+            width={700}
+            height={500}
+            alt="Picture of office"
+          />
+          <p>
+            Aliquet nec ullamcorper sit amet risus nullam. Purus in mollis nunc
+            sed id semper risus in. Purus sit amet volutpat consequat mauris
+            nunc congue nisi. Pharetra et ultrices neque ornare aenean euismod.
+            Enim diam vulputate ut pharetra sit amet aliquam id diam. Ut eu sem
+            integer vitae justo. Aliquet nec ullamcorper sit amet risus nullam.
+          </p>
+        </div>
+        <div>
+          <h5 className="dashed">Our Features</h5>
+          <h3>Find balance & well being today!</h3>
+          <Image
+            src="/features.jpg"
+            width={600}
+            height={400}
+            alt="Picture of features"
+          />
+        </div>
+      </div>
+
+      <div className="elementor-populated">
+        <div>
+          <h5 className="dashed">Our gallery</h5>
+          <h3>Transform with theraphy, galleries showcasing healing images</h3>
+        </div>
+        <div className="elementor-button-text">
+          <p>
+            Aliquet nec ullamcorper sit amet risus nullam. Purus in mollis{" "}
+            <br />
+            nunc sed id semper risus in. Purus sit amet volutpat consequat
+            mauris nunc congue nisi. Pharetra et ultrices neque ornare aenean
+            euismod.
+          </p>
+          <a href="#">Read More</a>
+        </div>
+      </div>
+
+      <div className="gallery-image">
         <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          src="/gallery/gallery-1.jpg"
+          width={250}
+          height={200}
+          alt="picture of gallery"
+        />
+        <Image
+          src="/gallery/gallery-2.jpg"
+          width={300}
+          height={200}
+          alt="picture of gallery"
+        />
+        <Image
+          src="/gallery/gallery-3.jpg"
+          width={300}
+          height={200}
+          alt="picture of gallery"
+        />
+        <Image
+          src="/gallery/gallery-4.jpg"
+          width={300}
+          height={200}
+          alt="picture of gallery"
+        />
+        <Image
+          src="/gallery/gallery-5.jpg"
+          width={300}
+          height={200}
+          alt="picture of gallery"
         />
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+      <div className="elementor-iconbox-description">
+        <hr className="elementor-title" />
+        <br />
+        <span>Our blog</span>
+        <p>
+          Find balance & well-being with our <br /> personalized therapies
+        </p>
+      </div>
+      <div className="blog-image">
+        <div>
+          <Image
+            src="/blog/blog-1.jpg"
+            width={400}
+            height={300}
+            alt="blog image"
+          />
+          <div className="blog-image-content">
+            <a className="blog-image-heading">
+              Therapy helps self-awareness and <br /> growth by exploring
+              emotions
+            </a>
+            <p className="blog-image-para">August 30, 2023 /// No comments</p>
+            <p className="blog-image-paragraph">
+              Enim ut tellus elementum sagittis vitae et. Vulputate dignissim
+              suspendisse in est ante in. Elementum sagittis vitae et leo.
+              Malesuada proin libero nunc consequat interdum
+            </p>
+            <a className="blog-image-read" href="#">
+              Read More
+            </a>
+          </div>
+        </div>
+        <div>
+          <Image
+            src="/blog/blog-2.jpg"
+            width={400}
+            height={300}
+            alt="blog image"
+          />
+          <div className="blog-image-content">
+            <a className="blog-image-heading">
+              Controlling anxiety: how therapy can help reduce symptoms
+            </a>
+            <p className="blog-image-para">August 30, 2023 /// No comments</p>
+            <p className="blog-image-paragraph">
+              Enim ut tellus elementum sagittis vitae et. Vulputate dignissim
+              suspendisse in est ante in. Elementum sagittis vitae et leo.
+              Malesuada proin libero nunc consequat interdum
+            </p>
+            <a className="blog-image-read" href="#">
+              Read More
+            </a>
+          </div>
+        </div>
+        <div>
+          <Image
+            src="/blog/blog-3.jpg"
+            width={400}
+            height={300}
+            alt="blog image"
+          />
+          <div className="blog-image-content">
+            <a className="blog-image-heading">
+              Therapy helps self-awareness and <br /> growth by exploring
+              emotions
+            </a>
+            <p className="blog-image-para">August 30, 2023 /// No comments</p>
+            <p className="blog-image-paragraph">
+              Enim ut tellus elementum sagittis vitae et. Vulputate dignissim
+              suspendisse in est ante in. Elementum sagittis vitae et leo.
+              Malesuada proin libero nunc consequat interdum
+            </p>
+            <a className="blog-image-read" href="#">
+              Read More
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="elementor-top-section">
+        <div className="elementor-top-content">
           <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
+            Take care of your mental health & find balance <br /> with help of
+            psychology!
           </h2>
           <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+            Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
           </p>
-        </a>
+        </div>
+        <div className="elementor-button-wrapper">
+          <button className="elementor-top-button">Contact Us</button>
+        </div>
       </div>
-    </main>
+
+      <div class="footer">
+        <div class="content">
+          <div class="services">
+            <h4>Pages</h4>
+            <p>
+              <a href="#">About Us</a>
+            </p>
+            <p>
+              <a href="#">Contact Us</a>
+            </p>
+            <p>
+              <a href="#">Faq</a>
+            </p>
+            <p>
+              <a href="#">Team</a>
+            </p>
+          </div>
+          <div class="social-media">
+            <h4>Company</h4>
+            <p>
+              <a href="#">Linkedin</a>
+            </p>
+            <p>
+              <a href="#">Twitter</a>
+            </p>
+            <p>
+              <a href="#">Github</a>
+            </p>
+            <p>
+              <a href="#">Facebook</a>
+            </p>
+            <p>
+              <a href="#">Instagram</a>
+            </p>
+          </div>
+          <div class="links">
+            <h4>Quick links</h4>
+            <p>
+              <a href="#">Home</a>
+            </p>
+            <p>
+              <a href="#">About</a>
+            </p>
+            <p>
+              <a href="#">Blogs</a>
+            </p>
+            <p>
+              <a href="#">Contact</a>
+            </p>
+          </div>
+          <div class="details">
+            <h4 class="address">Address</h4>
+            <p>
+              Lorem ipsum dolor sit amet consectetur <br />
+              adipisicing elit. Cupiditate, qui!
+            </p>
+            <h4 class="mobile">Mobile</h4>
+            <p>
+              <a href="#">+91-9836576571</a>
+            </p>
+            <h4 class="mail">Email</h4>
+            <p>
+              <a href="#">physiotheraphy@gmail.com</a>
+            </p>
+          </div>
+        </div>
+        <footer>
+          <hr />© 2022 physiotheraphy.
+        </footer>
+      </div>
+    </div>
   );
 }
